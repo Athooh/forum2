fad831f46875c8d296e3f6d10e3fb3b4ef6eb6ee
oathooh <oathooh@gmail.com>
2024-11-28 21:54:35 +0300

    Update: solved the page rendering problem. the content are now loaded with the right url path

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,56 +1,63 @@
 package handlers
 
 import (
-    "fmt"
-    "net/http"
-    "text/template"
+	"fmt"
+	"net/http"
+	"text/template"
 )
 
 var templates *template.Template
 
 func init() {
-    // Parse all templates in one go
-    templates = template.Must(template.ParseFiles(
-        "templates/base.html",
-        "templates/login.html",
-        "templates/signup.html",
-    ))
+	// Parse all templates in one go
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+
+	// Print actual template names
+	for _, t := range templates.Templates() {
+		fmt.Println("Parsed template name:", t.Name())
+	}
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-    // Redirect to login if accessing root directly
-    if r.URL.Path == "/" {
-        http.Redirect(w, r, "/login", http.StatusSeeOther)
-        return
-    }
+	// Redirect to login if accessing root directly
+	if r.URL.Path == "/" {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("LoginHandler invoked")
+	fmt.Println("LoginHandler invoked")
 
-    data := map[string]interface{}{
-        "Title": "Login - Forum",
-    }
+	// List all available templates
+	// fmt.Println("Available templates:", templates.Templates())
 
-    err := templates.ExecuteTemplate(w, "base", data)
-    if err != nil {
-        fmt.Printf("Error executing login template: %v\n", err)
-        http.Error(w, "Error rendering template: "+err.Error(), http.StatusInternalServerError)
-        return
-    }
+	data := map[string]interface{}{
+		"Title": "Login - Forum",
+	}
+
+	err := templates.ExecuteTemplate(w, "login", data)
+	if err != nil {
+		fmt.Printf("Error executing login template: %v\n", err)
+		http.Error(w, "Error rendering template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("SignupHandler invoked")
+	fmt.Println("SignupHandler invoked")
 
-    data := map[string]interface{}{
-        "Title": "Sign Up - Forum",
-    }
+	// List all available templates
+	// fmt.Println("Available templates:", templates.Templates())
 
-    err := templates.ExecuteTemplate(w, "base", data)
-    if err != nil {
-        fmt.Printf("Error executing signup template: %v\n", err)
-        http.Error(w, "Error rendering template: "+err.Error(), http.StatusInternalServerError)
-        return
-    }
+	data := map[string]interface{}{
+		"Title": "Login - Forum",
+	}
+
+	err := templates.ExecuteTemplate(w, "signup", data)
+	if err != nil {
+		fmt.Printf("Error executing signup template: %v\n", err)
+		http.Error(w, "Error rendering template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
